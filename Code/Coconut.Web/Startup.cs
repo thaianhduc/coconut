95e43eb31e07a0531ec7be39906874c5fdd60448
Thai Anh Duc <anhduc.thai@gmail.com>
2017-06-08 13:48:13 +0700

    Remove the use of MVC to simplify the demo

--- Code/Coconut.Web/Startup.cs
+++ Code/Coconut.Web/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +9,14 @@ namespace Coconut.Web
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            /*var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath);
+
+            var configuration = builder.Build();
+            var appKey = configuration["appConfig_Hello"];*/
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,10 +32,10 @@ namespace Coconut.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            //app.UseMvc();
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync($"You are requesting {context.Request.Path}");
             });
         }
     }
