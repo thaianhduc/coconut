ee69bce0bfcc154e4c2cf8e71f09cae136309698
Thai Anh Duc <anhduc.thai@gmail.com>
2017-06-16 20:50:49 +0700

    App config in JSON file. Finish the experiment and ready to deploy to Azure

---
+++ Code/Coconut.Web/AppSettings.cs
@@ -0,0 +1,20 @@
+namespace Coconut.Web
+{
+    public class AppSettings
+    {
+        public string Message { get; set; }
+        public ConnectionStrings ConnectionStrings { get; set; }
+        public Author Author { get; set; }
+    }
+    public class ConnectionStrings
+    {
+        public string Ef { get; set; }
+    }
+
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+    }
+
+}
--- Code/Coconut.Web/Startup.cs
+++ Code/Coconut.Web/Startup.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Coconut.Web
 {
+    public class AppSettingsController
+    {
+        private readonly AppSettings _appSettings;
+        public AppSettingsController(IOptions<AppSettings> appSettingsOptions)
+        {
+            _appSettings = appSettingsOptions.Value;
+        }
+
+        public Task GetAppSettings(HttpContext context)
+        {
+            return context.Response.WriteAsync($"Thie is AppSettingsController: {_appSettings.Message}; " +
+                                               $"EF Connection String: {_appSettings.ConnectionStrings.Ef}; " +
+                                               $"Author: {_appSettings.Author.Name}");
+        }
+    }
     public class Startup
     {
         private readonly IConfigurationRoot _configuration;
@@ -14,14 +32,19 @@ namespace Coconut.Web
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appSettings.json");
+                .AddJsonFile("appSettings.json")
+                .AddJsonFile("appComplexSettings.json");
 
             _configuration = builder.Build();
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the ability to read options in configuration
+            services.AddOptions();
+            services.Configure<AppSettings>(_configuration);
+
+            // Register a controller with a lifestyle of httprequest scoped
+            services.AddScoped<AppSettingsController>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,10 +57,13 @@ namespace Coconut.Web
                 app.UseDeveloperExceptionPage();
             }
             //app.UseMvc();
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync($"{_configuration["Message"]}. And you are? {context.Request.Path}");
-            });
+            app.Run(HandleRequest);
+        }
+
+        public Task HandleRequest(HttpContext context)
+        {
+           var controller = context.RequestServices.GetRequiredService<AppSettingsController>();
+            return controller.GetAppSettings(context);
         }
     }
 }
