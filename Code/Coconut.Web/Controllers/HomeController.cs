e0161cd941a7009c03a2705822e304d41dbca84e
Thai Anh Duc <anhduc.thai@gmail.com>
2017-07-18 22:06:56 +0700

    Implement the UglyFact action with business logic inside the Controller Action

--- Code/Coconut.Web/Controllers/HomeController.cs
+++ Code/Coconut.Web/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
+using Coconut.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Coconut.Web.Controllers
@@ -11,6 +10,25 @@ namespace Coconut.Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Serve the GET /home/uglyfact?birthday=dd-MM-yyyy
+        /// </summary>
+        /// <param name="birthday"></param>
+        /// <returns></returns>
+        public IActionResult UglyFact(string birthday)
+        {
+            // Ignore all data validation. Simply assume that the request is correct.
+            var dob = DateTime.ParseExact(birthday, "dd-MM-yyyy", CultureInfo.CurrentCulture);
+            var meetCreatorOn = dob.AddYears(80);
+            var spent = (int)Math.Round(DateTime.Now.Subtract(dob).TotalDays / 7);
+            var closed = (int)Math.Round(meetCreatorOn.Subtract(DateTime.Now).TotalDays / 7);
+            return View( new UglyFactModel
+            {
+                SpentWeeks = spent,
+                CloseToCreatorWeeks = closed
+            });
         }
     }
 }
---
+++ Code/Coconut.Web/Models/UglyFactModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Coconut.Web.Models
+{
+    public class UglyFactModel
+    {
+        public int SpentWeeks { get; set; }
+        public int CloseToCreatorWeeks { get; set; }
+    }
+}
